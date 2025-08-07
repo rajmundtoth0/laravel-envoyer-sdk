9f3704200de18955cf94f3d1a60a411b1203b5a5
rajmundtoth0 <rajm.toth@gmail.com>
2025-08-07 05:51:40 +0200

    Implement refresh method in Server class

--- src/Resources/Server.php
+++ src/Resources/Server.php
@@ -5,4 +5,28 @@ namespace JustSteveKing\Laravel\Envoyer\SDK\Resources;
 class Server extends EnvoyerResource
 {
     protected string $path = 'servers';
+
+        public function refresh(int $identifier):? object
+    {
+        $this->uri()->addPath(
+            "{$this->uri()->path()}/{$identifier}/refresh"
+        );
+
+        try {
+            $response = $this->http()->post(
+                $this->uri()->toString(),
+                [],
+                $this->strategy()->getHeader($this->authHeader)
+            );
+            // @codeCoverageIgnoreStart
+        } catch (\Exception $e) {
+            throw $e;
+        }
+        // @codeCoverageIgnoreEnd
+
+        return \json_decode(
+            $response->getBody()->getContents(),
+            false
+        );
+    }
 }
